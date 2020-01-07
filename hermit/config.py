f1ec290d7e93140be81b4f29ac00516d32fb52cc
Chris Howe <chris@howeville.com>
2020-01-07 13:45:34 -0600

    Removed all references to the 'wallet_words_file' config parameter, replacing it with 'shards_file' where appropriate.

--- hermit/config.py
+++ hermit/config.py
@@ -11,7 +11,6 @@ class HermitConfig:
 
     The following settings are supported:
 
-    * `wallet_words_file` -- path to store wallet words
     * `shards_file` -- path to store shards
     * `plugin_dir` -- directory containing plugins
     * `commands` -- a dictionary of command lines used to manipulate storage, see :attribute:`hermit.HermitConfig.DefaultCommands`.
@@ -30,7 +29,7 @@ class HermitConfig:
     #: * `getPersistedShards` -- copy from persistent storage to file system
     #: * `backupShards` -- copy from file system to backup storage
     #: * `restoreBackup` -- copy from backup storage to file system
-    #: 
+    #:
     DefaultCommands = {
         'persistShards': "cat {0} | gzip -c - > {0}.persisted",
         'backupShards': "cp {0}.persisted {0}.backup",
@@ -40,7 +39,6 @@ class HermitConfig:
 
     DefaultPaths = {
         'config_file':  '/etc/hermit.yaml',
-        'wallet_words_file': '/tmp/wallet_words.json',
         'shards_file': '/tmp/shard_words.bson',
         'plugin_dir': '/var/lib/hermit',
     }
@@ -54,17 +52,14 @@ class HermitConfig:
         """
 
         self.config_file = config_file
-        self.wallet_words_file = self.DefaultPaths['wallet_words_file']
         self.shards_file = self.DefaultPaths['shards_file']
         self.plugin_dir = self.DefaultPaths['plugin_dir']
         self.config: Dict = {}
         self.commands: Dict = {}
-        
+
         if path.exists(config_file):
             self.config = yaml.safe_load(open(config_file))
 
-        if 'wallet_words_file' in self.config:
-            self.wallet_words_file = self.config['wallet_words_file']
         if 'shards_file' in self.config:
             self.shards_file = self.config['shards_file']
         if 'plugin_dir' in self.config:
@@ -79,7 +74,7 @@ class HermitConfig:
                 self.commands[key] = defaults[key]
 
         for key in self.commands:
-            formatted_key = self.commands[key].format(self.wallet_words_file)
+            formatted_key = self.commands[key].format(self.shards_file)
             self.commands[key] = formatted_key
 
     @classmethod
