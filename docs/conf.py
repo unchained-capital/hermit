fd462bb1fedc4768367c67bd46b76d353080ab5d
Dhruv Bansal <shrub.vandal@gmail.com>
2021-10-06 15:34:53 -0500

    Add flax as author

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")
 
 project = "Hermit"
 copyright = "2019, Unchained Capital"
-author = "Chris Howe, Destry Saul, Dhruv Bansal"
+author = "Chris Howe, Destry Saul, Dhruv Bansal, Michael Flaxman"
 
 # The short X.Y version
 version = "0.1"
