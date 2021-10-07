f1947a99090690278c0de59fbb51224171103578
Dhruv Bansal <shrub.vandal@gmail.com>
2021-10-07 16:07:50 -0500

    Fix IO in opencv on mac

--- hermit/qrcode/opencvqrdisplay.py
+++ hermit/qrcode/opencvqrdisplay.py
@@ -7,7 +7,6 @@ class OpenCVQRDisplay(QRDisplay):
     def __init__(self, qr_config):
         self.x_position = int(qr_config.get('x_position', 100))
         self.y_position = int(qr_config.get('y_position', 100))
-        self.winname = "readqr"
 
     def animate_qrs(self, qrs: list) -> None:
         # Build images to display before we show the window.
@@ -29,28 +28,31 @@ class OpenCVQRDisplay(QRDisplay):
                     cv2.setWindowTitle(winname, f"QR Code {index+1} of {total}")
 
                     # Allow the window to update and wait for 200ms for a keypress
+                    # if not True:
+                    cv2.waitKey(1)
                     if not window_is_open(winname,200):
                         finished = True
                         break
 
         finally:
             cv2.destroyWindow(winname)
+            cv2.waitKey(1)
 
 
     def setup_camera_display(self):
         # Dont include a toolbar or a status bar at the bottom.
-        cv2.namedWindow(self.winname, cv2.WINDOW_NORMAL | cv2.WINDOW_GUI_NORMAL)
+        cv2.namedWindow("readqr", cv2.WINDOW_NORMAL | cv2.WINDOW_GUI_NORMAL)
 
         # Move the window to a sane place on the screen
-        cv2.moveWindow(self.winname, self.x_position, self.y_position)
+        cv2.moveWindow("readqr", self.x_position, self.y_position)
 
     def teardown_camera_display(self):
-        cv2.destroyWindow(self.winname)
-
+        cv2.destroyWindow("readqr")
+        cv2.waitKey(1)
 
     def display_camera_image(self, image):
         cvimg = np.array(image)
         # RGB to BGR
         cvimg = cvimg[:, :, ::-1].copy()
-        cv2.imshow(self.winname, cvimg)
-        return window_is_open(self.winname, 100)
+        cv2.imshow("readqr", cvimg)
+        return window_is_open("readqr", 100)
