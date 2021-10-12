057e75770670b132fc0452742c408c9ebc33f45b
Chris Howe <chris@unchained-capital.com>
2021-10-12 16:28:34 -0500

    Switched to secp256k1 coordinator signatures
    Added a script to rewrite psbts in 'buidl' normal order.
    
    Added normalized and signed versions of the 2-of-2.p2sh psbt in the fixtures.

--- hermit/coordinator.py
+++ hermit/coordinator.py
@@ -1,9 +1,6 @@
 from typing import Tuple
 
 from buidl import PSBT
-from Crypto.Hash import SHA256
-from Crypto.Signature import PKCS1_v1_5
-from Crypto.PublicKey import RSA
 
 from .config import get_config
 from .errors import InvalidCoordinatorSignature
@@ -41,8 +38,8 @@ def validate_coordinator_signature_if_necessary(original_psbt: PSBT) -> None:
         else:
             return
 
-    unsigned_psbt_base64_bytes, sig_bytes = extract_rsa_signature_params(original_psbt)
-    validate_rsa_signature(unsigned_psbt_base64_bytes, sig_bytes)
+    unsigned_psbt_base64_bytes, sig_bytes = extract_signature_params(original_psbt)
+    validate_secp256k1_signature(unsigned_psbt_base64_bytes, sig_bytes)
 
 
 def create_secp256k1_signature(message: bytes, private_key_path: str) -> bytes:
@@ -56,55 +53,6 @@ def create_secp256k1_signature(message: bytes, private_key_path: str) -> bytes:
 
     signature = private_key.sign_message(message)
     return signature.der()
-
-
-def create_rsa_signature(message: bytes, private_key_path: str) -> bytes:
-    """Create an RSA signature.
-
-    This function is not called within usual Hermit operation.  It is
-    useful for scripts and tests.
-
-    """
-
-    with open(private_key_path, mode="r") as private_key_file:
-        private_key = RSA.importKey(private_key_file.read())
-
-    digest = SHA256.new()
-    digest.update(message)
-    signer = PKCS1_v1_5.new(private_key)
-    signature = signer.sign(digest)
-    return signature
-
-
-def validate_rsa_signature(message: bytes, signature: bytes) -> None:
-    """Validate an RSA signature.
-
-    Uses the public key from Hermit's configuration for verification
-    (see :attr:`~hermit.config.DefaultCoordinator`).
-
-    Will raise :class:`~hermit.errors.InvalidCoordinatorSignature` if
-    the public key is missing or invalid or if the signature is
-    invalid.
-
-    """
-    public_key_text = get_config().coordinator.get("public_key")
-    if public_key_text is None:
-        raise InvalidCoordinatorSignature(
-            "Coordinator signature is present but no public key is configured."
-        )
-
-    try:
-        public_key = RSA.importKey(public_key_text)
-    except Exception:
-        raise InvalidCoordinatorSignature(
-            "Coordinator signature is present but coordinator public key is invalid."
-        )
-
-    digest = SHA256.new()
-    digest.update(message)
-    verifier = PKCS1_v1_5.new(public_key)
-    if not verifier.verify(digest, signature):  # type: ignore
-        raise InvalidCoordinatorSignature("Coordinator signature is invalid.")
 
 
 def validate_secp256k1_signature(message: bytes, signature: bytes) -> None:
@@ -138,10 +86,6 @@ def validate_secp256k1_signature(message: bytes, signature: bytes) -> None:
         raise InvalidCoordinatorSignature("Coordinator signature is invalid.")
 
 
-def extract_rsa_signature_params(original_psbt: PSBT) -> Tuple[bytes, bytes]:
-    return extract_signature_params(original_psbt)
-
-
 def extract_signature_params(original_psbt: PSBT) -> Tuple[bytes, bytes]:
     """Extract signature parameters from a PSBT.
 
@@ -163,24 +107,6 @@ def extract_signature_params(original_psbt: PSBT) -> Tuple[bytes, bytes]:
     unsigned_psbt_base64_bytes = unsigned_psbt_base64.encode("utf8")
 
     return unsigned_psbt_base64_bytes, sig_bytes
-
-
-def add_rsa_signature(original_psbt: PSBT, private_key_path: str) -> PSBT:
-    """Add a signature to a PSBT.
-
-    This is useful for scripts and tests, but not actually ever called in
-    the course of regular Hermit operation
-    """
-
-    psbt_base64 = original_psbt.serialize_base64()
-
-    sig_bytes = create_rsa_signature(
-        bytes(psbt_base64, "utf-8"),
-        private_key_path,
-    )
-
-    original_psbt.extra_map[COORDINATOR_SIGNATURE_KEY] = sig_bytes
-    return original_psbt
 
 
 def add_secp256k1_signature(original_psbt: PSBT, private_key_path: str) -> PSBT:
--- tests/test_coordinator.py
+++ tests/test_coordinator.py
@@ -1,14 +1,11 @@
 from unittest.mock import Mock, patch
 from pytest import raises
-from buidl import PrivateKey, PSBT
+from buidl import PSBT
 
 from hermit import InvalidCoordinatorSignature
 from hermit.coordinator import (
     validate_coordinator_signature_if_necessary,
-    validate_rsa_signature,
-    create_rsa_signature,
-    add_rsa_signature,
-    extract_rsa_signature_params,
+    extract_signature_params,
     create_secp256k1_signature,
     add_secp256k1_signature,
     validate_secp256k1_signature,
@@ -44,12 +41,12 @@ class TestValidateCoordinatorSignatureIfNecessary(object):
         assert "signature is missing" in str(e)
         mock_get_config.assert_called_once_with()
 
-    @patch("hermit.coordinator.extract_rsa_signature_params")
-    @patch("hermit.coordinator.validate_rsa_signature")
+    @patch("hermit.coordinator.extract_signature_params")
+    @patch("hermit.coordinator.validate_secp256k1_signature")
     def test_when_signature_valid_and_not_required(
         self,
-        mock_validate_rsa_signature,
-        mock_extract_rsa_signature_params,
+        mock_validate_secp256k1_signature,
+        mock_extract_signature_params,
         mock_get_config,
     ):
         mock_get_config.return_value = self.config
@@ -57,21 +54,21 @@ class TestValidateCoordinatorSignatureIfNecessary(object):
 
         self.extra_map[COORDINATOR_SIGNATURE_KEY] = self.signature
 
-        mock_extract_rsa_signature_params.return_value = (self.message, self.signature)
+        mock_extract_signature_params.return_value = (self.message, self.signature)
 
         validate_coordinator_signature_if_necessary(self.psbt)
         mock_get_config.assert_called_once_with()
-        mock_extract_rsa_signature_params.assert_called_once_with(self.psbt)
-        mock_validate_rsa_signature.assert_called_once_with(
+        mock_extract_signature_params.assert_called_once_with(self.psbt)
+        mock_validate_secp256k1_signature.assert_called_once_with(
             self.message, self.signature
         )
 
-    @patch("hermit.coordinator.extract_rsa_signature_params")
-    @patch("hermit.coordinator.validate_rsa_signature")
+    @patch("hermit.coordinator.extract_signature_params")
+    @patch("hermit.coordinator.validate_secp256k1_signature")
     def test_when_signature_valid_and_required(
         self,
-        mock_validate_rsa_signature,
-        mock_extract_rsa_signature_params,
+        mock_validate_secp256k1_signature,
+        mock_extract_signature_params,
         mock_get_config,
     ):
         mock_get_config.return_value = self.config
@@ -79,21 +76,21 @@ class TestValidateCoordinatorSignatureIfNecessary(object):
 
         self.extra_map[COORDINATOR_SIGNATURE_KEY] = self.signature
 
-        mock_extract_rsa_signature_params.return_value = (self.message, self.signature)
+        mock_extract_signature_params.return_value = (self.message, self.signature)
 
         validate_coordinator_signature_if_necessary(self.psbt)
         mock_get_config.assert_called_once_with()
-        mock_extract_rsa_signature_params.assert_called_once_with(self.psbt)
-        mock_validate_rsa_signature.assert_called_once_with(
+        mock_extract_signature_params.assert_called_once_with(self.psbt)
+        mock_validate_secp256k1_signature.assert_called_once_with(
             self.message, self.signature
         )
 
-    @patch("hermit.coordinator.extract_rsa_signature_params")
-    @patch("hermit.coordinator.validate_rsa_signature")
+    @patch("hermit.coordinator.extract_signature_params")
+    @patch("hermit.coordinator.validate_secp256k1_signature")
     def test_when_signature_invalid_and_not_required(
         self,
-        mock_validate_rsa_signature,
-        mock_extract_rsa_signature_params,
+        mock_validate_secp256k1_signature,
+        mock_extract_signature_params,
         mock_get_config,
     ):
         mock_get_config.return_value = self.config
@@ -101,9 +98,9 @@ class TestValidateCoordinatorSignatureIfNecessary(object):
 
         self.extra_map[COORDINATOR_SIGNATURE_KEY] = self.signature
 
-        mock_extract_rsa_signature_params.return_value = (self.message, self.signature)
+        mock_extract_signature_params.return_value = (self.message, self.signature)
 
-        mock_validate_rsa_signature.side_effect = InvalidCoordinatorSignature(
+        mock_validate_secp256k1_signature.side_effect = InvalidCoordinatorSignature(
             "Invalid signature."
         )
 
@@ -112,17 +109,17 @@ class TestValidateCoordinatorSignatureIfNecessary(object):
 
         assert "Invalid signature" in str(e)
         mock_get_config.assert_called_once_with()
-        mock_extract_rsa_signature_params.assert_called_once_with(self.psbt)
-        mock_validate_rsa_signature.assert_called_once_with(
+        mock_extract_signature_params.assert_called_once_with(self.psbt)
+        mock_validate_secp256k1_signature.assert_called_once_with(
             self.message, self.signature
         )
 
-    @patch("hermit.coordinator.extract_rsa_signature_params")
-    @patch("hermit.coordinator.validate_rsa_signature")
+    @patch("hermit.coordinator.extract_signature_params")
+    @patch("hermit.coordinator.validate_secp256k1_signature")
     def test_when_signature_invalid_and_required(
         self,
-        mock_validate_rsa_signature,
-        mock_extract_rsa_signature_params,
+        mock_validate_secp256k1_signature,
+        mock_extract_signature_params,
         mock_get_config,
     ):
         mock_get_config.return_value = self.config
@@ -130,9 +127,9 @@ class TestValidateCoordinatorSignatureIfNecessary(object):
 
         self.extra_map[COORDINATOR_SIGNATURE_KEY] = self.signature
 
-        mock_extract_rsa_signature_params.return_value = (self.message, self.signature)
+        mock_extract_signature_params.return_value = (self.message, self.signature)
 
-        mock_validate_rsa_signature.side_effect = InvalidCoordinatorSignature(
+        mock_validate_secp256k1_signature.side_effect = InvalidCoordinatorSignature(
             "Invalid signature."
         )
 
@@ -141,36 +138,36 @@ class TestValidateCoordinatorSignatureIfNecessary(object):
 
         assert "Invalid signature" in str(e)
         mock_get_config.assert_called_once_with()
-        mock_extract_rsa_signature_params.assert_called_once_with(self.psbt)
-        mock_validate_rsa_signature.assert_called_once_with(
+        mock_extract_signature_params.assert_called_once_with(self.psbt)
+        mock_validate_secp256k1_signature.assert_called_once_with(
             self.message, self.signature
         )
 
 
 @patch("hermit.coordinator.get_config")
-def test_create_rsa_sigature(mock_get_config):
-    public_key = open("tests/fixtures/coordinator.pub", "r").read()
-    private_key_path = "tests/fixtures/coordinator.pem"
+def test_create_secp256k1_sigature(mock_get_config):
+    public_key = open("tests/fixtures/coordinator.pubkey", "r").read().strip()
+    private_key_path = "tests/fixtures/coordinator.privkey"
 
     message = "Hello there".encode("utf8")
-    signature = create_rsa_signature(message, private_key_path)
+    signature = create_secp256k1_signature(message, private_key_path)
 
     config = Mock()
     coordinator_config = dict(public_key=public_key)
     config.coordinator = coordinator_config
     mock_get_config.return_value = config
-    validate_rsa_signature(message, signature)
+    validate_secp256k1_signature(message, signature)
     mock_get_config.assert_called_once_with()
 
 
 @patch("hermit.coordinator.get_config")
-class TestValidateRSASignature(object):
+class TestValidateSECP256K1Signature(object):
     def setup(self):
-        self.public_key = open("tests/fixtures/coordinator.pub", "r").read()
-        self.private_key_path = "tests/fixtures/coordinator.pem"
+        self.public_key = open("tests/fixtures/coordinator.pubkey", "r").read().strip()
+        self.private_key_path = "tests/fixtures/coordinator.privkey"
 
         self.message = "Hello there".encode("utf8")
-        self.signature = create_rsa_signature(self.message, self.private_key_path)
+        self.signature = create_secp256k1_signature(self.message, self.private_key_path)
 
         self.config = Mock()
         self.coordinator_config = dict(public_key=self.public_key)
@@ -180,7 +177,7 @@ class TestValidateRSASignature(object):
         del self.coordinator_config["public_key"]
         mock_config.return_value = self.config
         with raises(InvalidCoordinatorSignature) as e:
-            validate_rsa_signature(self.message, self.signature)
+            validate_secp256k1_signature(self.message, self.signature)
         assert "no public key is configured" in str(e)
         mock_config.assert_called_once_with()
 
@@ -188,63 +185,63 @@ class TestValidateRSASignature(object):
         self.coordinator_config["public_key"] = "foobar"
         mock_config.return_value = self.config
         with raises(InvalidCoordinatorSignature) as e:
-            validate_rsa_signature(self.message, self.signature)
+            validate_secp256k1_signature(self.message, self.signature)
         assert "public key is invalid" in str(e)
         mock_config.assert_called_once_with()
 
     def test_when_signature_is_valid(self, mock_config):
         mock_config.return_value = self.config
-        validate_rsa_signature(self.message, self.signature)
+        validate_secp256k1_signature(self.message, self.signature)
         mock_config.assert_called_once_with()
 
     def test_when_signature_is_invalid(self, mock_config):
         mock_config.return_value = self.config
         with raises(InvalidCoordinatorSignature) as e:
-            validate_rsa_signature(self.message + b"hello", self.signature)
+            validate_secp256k1_signature(self.message + b"hello", self.signature)
         mock_config.assert_called_once_with()
         assert "signature is invalid" in str(e)
 
 
-@patch("hermit.coordinator.get_config")
-class TestPSBTSignatureBasics(object):
-    def setup(self):
-        self.public_key = open("tests/fixtures/coordinator.pub", "r").read()
-        self.private_key_path = "tests/fixtures/coordinator.pem"
-
-        self.original_psbt_base64 = open(
-            "tests/fixtures/signature_requests/2-of-2.p2sh.testnet.psbt", "r"
-        ).read()
-
-        self.psbt = PSBT.parse_base64(self.original_psbt_base64)
-        self.psbt_base64 = self.psbt.serialize_base64()
-
-        self.config = Mock()
-        self.coordinator_config = dict(public_key=self.public_key)
-        self.config.coordinator = self.coordinator_config
-
-    def test_psbt_serialization_stable(self, mock_config):
-        mock_config.return_value = self.config
-
-        p2 = PSBT.parse_base64(self.psbt_base64)
-        assert p2.serialize_base64() == self.psbt.serialize_base64()
-
-    def test_psbt_signature(self, mock_config):
-        mock_config.return_value = self.config
-
-        signature = create_rsa_signature(
-            bytes(self.psbt_base64, "utf-8"), self.private_key_path
-        )
-        add_rsa_signature(self.psbt, self.private_key_path)
-
-        assert self.psbt.extra_map[COORDINATOR_SIGNATURE_KEY] == signature
-
-    def test_validate_psbt_signature(self, mock_config):
-        mock_config.return_value = self.config
-
-        add_rsa_signature(self.psbt, self.private_key_path)
-
-        unsigned_psbt_base64_bytes, sig_bytes = extract_rsa_signature_params(self.psbt)
-        validate_rsa_signature(unsigned_psbt_base64_bytes, sig_bytes)
+# @patch("hermit.coordinator.get_config")
+# class TestPSBTSignatureBasics(object):
+# def setup(self):
+# self.public_key = open("tests/fixtures/coordinator.pub", "r").read()
+# self.private_key_path = "tests/fixtures/coordinator.pem"
+#
+# self.original_psbt_base64 = open(
+# "tests/fixtures/signature_requests/2-of-2.p2sh.testnet.psbt", "r"
+# ).read()
+#
+# self.psbt = PSBT.parse_base64(self.original_psbt_base64)
+# self.psbt_base64 = self.psbt.serialize_base64()
+#
+# self.config = Mock()
+# self.coordinator_config = dict(public_key=self.public_key)
+# self.config.coordinator = self.coordinator_config
+#
+# def test_psbt_serialization_stable(self, mock_config):
+# mock_config.return_value = self.config
+#
+# p2 = PSBT.parse_base64(self.psbt_base64)
+# assert p2.serialize_base64() == self.psbt.serialize_base64()
+#
+# def test_psbt_signature(self, mock_config):
+# mock_config.return_value = self.config
+#
+# signature = create_rsa_signature(
+# bytes(self.psbt_base64, "utf-8"), self.private_key_path
+# )
+# add_rsa_signature(self.psbt, self.private_key_path)
+#
+# assert self.psbt.extra_map[COORDINATOR_SIGNATURE_KEY] == signature
+#
+# def test_validate_psbt_signature(self, mock_config):
+# mock_config.return_value = self.config
+#
+# add_rsa_signature(self.psbt, self.private_key_path)
+#
+# unsigned_psbt_base64_bytes, sig_bytes = extract_rsa_signature_params(self.psbt)
+# validate_rsa_signature(unsigned_psbt_base64_bytes, sig_bytes)
 
 
 @patch("hermit.coordinator.get_config")
@@ -290,5 +287,5 @@ class TestPSBTSignatureSecP256K1Basics(object):
 
         add_secp256k1_signature(self.psbt, self.private_key_path)
 
-        unsigned_psbt_base64_bytes, sig_bytes = extract_rsa_signature_params(self.psbt)
+        unsigned_psbt_base64_bytes, sig_bytes = extract_signature_params(self.psbt)
         validate_secp256k1_signature(unsigned_psbt_base64_bytes, sig_bytes)
