6eacd2bad07ae2cb4e1148a33499d923d966cf30
Chris Howe <chris@unchained-capital.com>
2021-10-27 12:12:54 -0500

    fixed the toolbar TESTNET and DEBUG displays for unlocked wallet

--- hermit/ui/toolbar.py
+++ hermit/ui/toolbar.py
@@ -1,8 +1,13 @@
 from .base import DeadTime
 import hermit.ui.state as state
 
-Bars = "#" * DeadTime + " " * DeadTime
 
+bar_len = DeadTime // 4
+chars = ['#', '=', '-', '.' ]
+Bars = [
+    chars[0] * (bar_len -1) + char + " " * bar_len
+    for char in chars
+]
 
 def bottom_toolbar():
     """Renders the bottom toolbar."""
@@ -18,10 +23,11 @@ def bottom_toolbar():
 
     b = DeadTime - state.Timeout
     if state.Wallet.unlocked():
-        wallet_status = "wallet UNLOCKED " + Bars[b : b + DeadTime]
+        bar = Bars[b % 4][b//4 : b//4 + bar_len]
+        wallet_status = "wallet UNLOCKED " + bar
     else:
         wallet_status = "wallet locked"
 
-    return "Hermit --- {0:<16} {1:>10} {2:>8}".format(
+    return "Hermit --- {0:<32} {1:>7} {2:>5}".format(
         wallet_status, testnet_status, debug_status
     )
